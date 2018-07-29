5afe3d18dff2d96374221ac9c133b6e780011726
Kara Schaefer <yvkschaefer@gmail.com>
2018-07-29 15:34:08 -0400

    templates

--- noteapp/app.py
+++ noteapp/app.py
@@ -1,8 +1,6 @@
 from flask import Flask
+from noteapp.views.index import bp as index_bp
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return "Hello World!"
+app.register_blueprint(index_bp)
